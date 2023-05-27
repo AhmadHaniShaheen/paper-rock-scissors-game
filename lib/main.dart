9afe2bcd111a3b1513b175f2b4279681fa96c859
ahmadhanialishaheen@gmail.com <ahmadhanialishaheen@gmail.com>
2023-05-27 17:27:45 +0300

    paper rock scissors game

---
+++ lib/main.dart
@@ -0,0 +1,222 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('paper rock scissors game'),
+          backgroundColor: Colors.blue,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftImage = 1;
+  int rightImage = 1;
+  Color color = Colors.white;
+  IconData icon = Icons.grading;
+
+  /*
+  img1= cut
+  img2= rook
+  img3= paper
+
+
+  white=equal
+  red= loss
+  green= yas
+   */
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: color,
+                size: 48,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          leftImage = Random().nextInt(3) + 1;
+                          rightImage = Random().nextInt(3) + 1;
+                          if (leftImage == 1 && rightImage == 1) {
+                            setState(() {
+                              icon = Icons.grading;
+                              color = Colors.white;
+                            });
+                          } else if (leftImage == 1 && rightImage == 2) {
+                            setState(() {
+                              icon = Icons.thumb_down;
+                              color = Colors.red;
+                            });
+                          } else if (leftImage == 1 && rightImage == 3) {
+                            setState(() {
+                              icon = Icons.thumb_up;
+                              color = Colors.green;
+                            });
+                          } else if (leftImage == 2 && rightImage == 1) {
+                            setState(() {
+                              icon = Icons.thumb_up;
+                              color = Colors.green;
+                            });
+                          } else if (leftImage == 2 && rightImage == 2) {
+                            setState(() {
+                              icon = Icons.grading;
+                              color = Colors.white;
+                            });
+                          } else if (leftImage == 2 && rightImage == 3) {
+                            setState(() {
+                              icon = Icons.thumb_down;
+                              color = Colors.red;
+                            });
+                          } else if (leftImage == 3 && rightImage == 1) {
+                            setState(() {
+                              icon = Icons.thumb_down;
+                              color = Colors.red;
+                            });
+                          } else if (leftImage == 3 && rightImage == 2) {
+                            setState(() {
+                              icon = Icons.thumb_up;
+                              color = Colors.green;
+                            });
+                          } else if (leftImage == 3 && rightImage == 3) {
+                            setState(() {
+                              icon = Icons.grading;
+                              color = Colors.white;
+                            });
+                          }
+                        });
+                      },
+                      child: Image(
+                        image: AssetImage('images/img$leftImage.png'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Text(
+                      'You',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 50,
+                child: Center(
+                  child: Text(
+                    'VS',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          leftImage = Random().nextInt(3) + 1;
+                          rightImage = Random().nextInt(3) + 1;
+                          if (leftImage == 1 && rightImage == 1) {
+                            setState(() {
+                              icon = Icons.grading;
+                              color = Colors.white;
+                            });
+                          } else if (leftImage == 1 && rightImage == 2) {
+                            setState(() {
+                              icon = Icons.thumb_down;
+                              color = Colors.red;
+                            });
+                          } else if (leftImage == 1 && rightImage == 3) {
+                            setState(() {
+                              icon = Icons.thumb_up;
+                              color = Colors.green;
+                            });
+                          } else if (leftImage == 2 && rightImage == 1) {
+                            setState(() {
+                              icon = Icons.thumb_up;
+                              color = Colors.green;
+                            });
+                          } else if (leftImage == 2 && rightImage == 2) {
+                            setState(() {
+                              icon = Icons.grading;
+                              color = Colors.white;
+                            });
+                          } else if (leftImage == 2 && rightImage == 3) {
+                            setState(() {
+                              icon = Icons.thumb_down;
+                              color = Colors.red;
+                            });
+                          } else if (leftImage == 3 && rightImage == 1) {
+                            setState(() {
+                              icon = Icons.thumb_down;
+                              color = Colors.red;
+                            });
+                          } else if (leftImage == 3 && rightImage == 2) {
+                            setState(() {
+                              icon = Icons.thumb_up;
+                              color = Colors.green;
+                            });
+                          } else if (leftImage == 3 && rightImage == 3) {
+                            setState(() {
+                              icon = Icons.grading;
+                              color = Colors.white;
+                            });
+                          }
+                        });
+                      },
+                      child: Image(
+                        image: AssetImage('images/img$rightImage.png'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Text(
+                      'System ',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
